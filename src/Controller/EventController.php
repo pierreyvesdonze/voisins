3118105ce2cdf92cd95144fc7231da0244b1bac1
py eye <pyd3.14@gmail.com>
2020-03-28 17:56:19 +0100

    back to date

--- src/Controller/EventController.php
+++ src/Controller/EventController.php
@@ -11,7 +11,6 @@ use Sensio\Bundle\FrameworkExtraBundle\Configuration\IsGranted;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\Notifier\NotifierInterface;
-use Twig\Extra\Intl\IntlExtension;
 use Symfony\Component\Routing\Annotation\Route;
 
 /**
